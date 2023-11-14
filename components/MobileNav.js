c41e2f4dd0693ea64f6fdf4058808d09f6ebf3d2
bimalmagar10 <bimalmagar873@gmail.com>
2023-11-14 23:40:05 +0545

    small padding and font-size fixes

--- components/MobileNav.js
+++ components/MobileNav.js
@@ -24,24 +24,24 @@ const MobileNav = () => {
 
 	return (
 		<>
-			<Box display={["block","block","none","none"]} width="100%" boxShadow={navbarShadow} padding="1.5rem 3rem" position="fixed" bottom="0" left="0" backgroundColor={bgColor}>
-				<Flex justifyContent="space-between">
+			<Box display={["block","block","none","none"]} width="100%" boxShadow={navbarShadow} padding={["1.2rem 3rem","1.2rem 6rem","1.2rem 6rem","1.2rem 6rem"]} position="fixed" bottom="0" left="0" backgroundColor={bgColor}>
+				<Flex justifyContent="space-between" alignItems="center">
 					<Link as={NextLink} href="/" textDecoration="unset !important">
 						<VStack align="center" spacing="0" color={uri === "/" ? activeColor : inactiveColor}>
-							<GoHomeFill fontSize="2.3rem"/>
-							<Text fontSize="1.6rem">Home</Text>
+							<GoHomeFill fontSize="2.1rem"/>
+							<Text fontSize="1.4rem">Home</Text>
 						</VStack>
 					</Link>
 					<Link as={NextLink} href="/blogs" textDecoration="unset !important">
 						<VStack align="center" color={regexForBlogsRoute.test(uri) ? activeColor : inactiveColor}>
-							<FaFilePen/>
-							<Text fontSize="1.6rem">Blogs</Text>
+							<FaFilePen fontSize="1.9rem"/>
+							<Text fontSize="1.4rem">Blogs</Text>
 						</VStack>
 					</Link>
 					<Link as={NextLink} href="/about-me" textDecoration="unset !important">
 						<VStack align="center" color={uri === "/about-me" ? activeColor : inactiveColor}>
-							<FaCircleUser/>
-							<Text fontSize="1.6rem">About Me</Text>
+							<FaCircleUser fontSize="2rem"/>
+							<Text fontSize="1.4rem">About Me</Text>
 						</VStack>
 					</Link>
 				</Flex>
