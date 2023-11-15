3363f2fa5120b014f4b43ca7182d27c1ff84d785
bimalmagar10 <bimalmagar873@gmail.com>
2023-11-15 14:35:10 +0545

    fix footer padding for mobile view

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ import {GridItems} from "../lib/helpers";
 const Footer = () => {
     const linkColor = useColorModeValue("grayish","gray.400");
 	return (
-     <SimpleGrid columns={[1,3]} spacing={10} p="0 0 4rem 0">
+     <SimpleGrid columns={[1,3]} spacing={10} p={["0 0 8rem 0","0 0 8rem 0","0 0 4rem 0","0 0 4rem 0"]}>
         {
         	GridItems.map((item,index) => (
 		        	<LinkItem as={Link}  href={item.url} key={index} fontSize="1.4rem" color={linkColor} target={item.url[0] === "/" ? "_self":"_blank"}>{item.name}</LinkItem>
